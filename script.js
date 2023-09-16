0023599e1758be6e460fb744e95db1f5fb69b160
Ahmed Essam El Sayed <144829480+ahmed514essam@users.noreply.github.com>
2023-09-16 14:41:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let username = 'Ahmed'
+let lastname = 'Essam'
+let age = 21
+
+
+
+const para = document.getElementsByTagName("p")
+
+console.log("para[0]")
+para[0].textContent = username
+
+
+
+console.log("para[1]")
+para[1].textContent = lastname
+
+
+
+console.log("para[2]")
+para[2].textContent = age
+
+
+
+var element = document.getElementById("my");
+element.classList.add("asx");
+
+
+
+const asx = document.querySelector("div.asx")
+asx.style.backgroundColor = ' red '
+
+
+
+
